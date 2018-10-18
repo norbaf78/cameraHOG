6bb558a6c5bb39076910b81a0b3cc0e07eb5c6e3
Fabio <fabio.roncato@trilogis.it>
2018-10-18 11:59:53 +0200

    update

--- camera.py
+++ camera.py
@@ -8,6 +8,10 @@ Created on Tue Oct 09 10:07:55 2018
 import numpy as np
 import json
 import cv2
+import pika
+import time
+from transform import CoordinateTransform
+from reprojecter import ReprojectTo4326
 
 def onclick(event, x, y, flags, param):
     #print("onclick !!" +  key + " " + id)
@@ -73,12 +77,14 @@ def rescale_heatmap_image_value(img):
 
 if __name__ == '__main__':
 
-    resize_img = 3 # the resize to the acquired image. The HOG will be evaluated on the resized image
-    additional_resize_point = 1.5 # the point in the image (source point for the homography) have been taken with resize_img=2. In case of
+    resize_img = 4 # the resize to the acquired image. The HOG will be evaluated on the resized image
+    additional_resize_point = 2 # the point in the image (source point for the homography) have been taken with resize_img=2. In case of
                                 # other resize change this vale (example 2,1 4,2 .....)
     cell_heatmap_step = 20
-    zoom_heatmap = 10.0
- 
+    zoom_heatmap = 4.0
+
+    transformer = CoordinateTransform()
+    reprojecter = ReprojectTo4326()
     with open('configReal.json') as json_data_file:
         data = json.load(json_data_file)  
     #point in source and destination images to create the Homography transformation
@@ -102,6 +108,17 @@ if __name__ == '__main__':
     pt_4_pix_src_image_Y = data['config']['pt_4_pix_src_image_Y']
     pt_4_pix_dst_image_X = data['config']['pt_4_pix_dst_image_X']
     pt_4_pix_dst_image_Y = data['config']['pt_4_pix_dst_image_Y']
+    #configuration of the connection to rabbitMQ    
+    host_rabbitmq = data['config']['host_rabbitmq']
+    host_rabbitmq_username = data['config']['host_rabbitmq_username']
+    host_rabbitmq_psw = data['config']['host_rabbitmq_psw']
+    #configuration key and id
+    key = data['config']['tile38_key']    
+    id = data['config']['tile38_id']
+    #image information
+    image_map_max_X = float(data['config']['backgound_image_max_X'])
+    image_map_max_Y = float(data['config']['backgound_image_max_Y'])
+
     
     print("pt_0_pix_src_image_X: " + str(pt_0_pix_src_image_X)) 
     print("pt_0_pix_src_image_Y: " + str(pt_0_pix_src_image_Y))    
@@ -123,15 +140,30 @@ if __name__ == '__main__':
     print("pt_4_pix_src_image_Y: " + str(pt_4_pix_src_image_Y))    
     print("pt_4_pix_dst_image_X: " + str(pt_4_pix_dst_image_X))    
     print("pt_4_pix_dst_image_Y: " + str(pt_4_pix_dst_image_Y))  
+    print("host_rabbitmq: " + host_rabbitmq)
+    print("host_rabbitmq_username: " + host_rabbitmq_username)    
+    print("host_rabbitmq_psw: " + host_rabbitmq_psw)
+    print("key: " + key)
+    print("id: " + id)
+    print("image_map_max_X: " + str(image_map_max_X))
+    print("image_map_max_Y: " + str(image_map_max_Y))
+
+    ####################################################  
+    #connect to rabbitmq
+    ####################################################
+    credentials = pika.PlainCredentials(host_rabbitmq_username, host_rabbitmq_psw)
+    parameters = pika.ConnectionParameters(host_rabbitmq,5672,'/',credentials, socket_timeout=10000000, heartbeat_interval=0,blocked_connection_timeout=300)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()    
 
 
-    img_map = cv2.imread('mapLaboratorio.jpg')   
+    img_map = cv2.imread('mapReal.png')  
     # https://zbigatron.com/mapping-camera-coordinates-to-a-2d-floor-plan/
     pts_src = np.array([[pt_0_pix_src_image_X/additional_resize_point, pt_0_pix_src_image_Y/additional_resize_point], [pt_1_pix_src_image_X/additional_resize_point, pt_1_pix_src_image_Y/additional_resize_point], [pt_2_pix_src_image_X/additional_resize_point, pt_2_pix_src_image_Y/additional_resize_point],[pt_3_pix_src_image_X/additional_resize_point, pt_3_pix_src_image_Y/additional_resize_point], [pt_4_pix_src_image_X/additional_resize_point, pt_4_pix_src_image_Y/additional_resize_point]])
     pts_dst = np.array([[pt_0_pix_dst_image_X, pt_0_pix_dst_image_Y], [pt_1_pix_dst_image_X, pt_1_pix_dst_image_Y], [pt_2_pix_dst_image_X, pt_2_pix_dst_image_Y],[pt_3_pix_dst_image_X, pt_3_pix_dst_image_Y], [pt_4_pix_dst_image_X, pt_4_pix_dst_image_Y]])
     h, status = cv2.findHomography(pts_src, pts_dst) # # calculate matrix H
     cv2.namedWindow('homography')
-    cv2.imshow('homography',img_map)  
+    #cv2.imshow('homography',img_map)  
 
 
     hog = cv2.HOGDescriptor()
@@ -143,6 +175,7 @@ if __name__ == '__main__':
 #    position_frame = cv2.resize(big_position_frame, (0,0), fx=1.0/resize_img, fy=1.0/resize_img) 
     #set the background image for the heatmap. Starting acquiring the dimension of the image where the elaboration have been made
     rows_map_frame,cols_map_frame, _ = img_map.shape 
+    img_map_view = np.zeros((cols_map_frame/4, rows_map_frame/4, 3), np.uint8)
     # rows_position_frame,cols_position_frame, _ = img_map.shape
     # ne channel iamge is the starting point for the heatmap
     heatmap_gray = np.zeros((rows_map_frame/cell_heatmap_step, cols_map_frame/cell_heatmap_step, 1), dtype = np.uint8)
@@ -163,12 +196,26 @@ if __name__ == '__main__':
         heatmap_gray = rescale_heatmap_image_value(heatmap_gray)        
         heatmap_color = cv2.applyColorMap(heatmap_gray, cv2.COLORMAP_JET)
         heatmap_color_resize_big = cv2.resize(heatmap_color, (0,0), fx=zoom_heatmap, fy=zoom_heatmap)
+        
+        #convert data in geographic position and provide the data to rabbitmq
+        for x, y, _, _  in found:
+            metersX,metersY = transformer.pixelToMeter(x,y,image_map_max_X,image_map_max_Y)            
+            newx,newy = transformer.transform(metersX,metersY)       
+            newy,newx = reprojecter.MetersToLatLon(newx,newy)
+            # insert data in queue in rabbitmq
+            body = '{"type":"Feature","geometry":{"type":"Point","coordinates":[' + str(newy) + ',' + str(newx) + ']},"properties":{"key":"' + key + '","id":"' + id + '","timestamp":"' + str(time.time()) + '"}}'
+           # channel.basic_publish(exchange='trilogis_exchange_pos',routing_key='trilogis_position',body=body, properties=pika.BasicProperties(delivery_mode = 2)) # make message persistent
+                  
                 
-        cv2.imshow('feed',frame)        
+        cv2.imshow('feed',frame)  
         cv2.imshow('heatmap',heatmap_color_resize_big) 
-        cv2.imshow('homography',img_map)
+        #cv2.imshow('homography',img_map)           
+        img_map_view = cv2.resize(img_map, (cols_map_frame/4,  rows_map_frame/4))
+        cv2.imshow('homography',img_map_view)
         ch = 0xFF & cv2.waitKey(1)
         if ch == 27:
             break
+    res = connection.close()
+    print 'rabbitmq connection close - ' + str(res)  
     cv2.destroyAllWindows()
     
